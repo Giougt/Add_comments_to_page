1e0c4bc08698d16b95303a4627e051aac76aec78
Giougt <alexandre18golf@gmail.com>
2024-12-29 22:05:47 +0100

    first comment

--- index.php
+++ index.php
@@ -9,9 +9,27 @@ include "public/assets/style.css"
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+     #input_text{
+    background-color: black;
+    color: white; 
+    height: 40px;
+}  </style>
 </head>
-    <div> Add a comment :</div>
-    <input type="text" placeholder="Taper du texte ici" id="input_text"> 
-    <button> Send </button>
+    <div>Ajouter un commentaire :</div>
+    <form method="post" action="">
+        <input type="text" placeholder="Taper du texte ici" name="input_text" id="input_text"> 
+        <button type="submit">Envoyer</button>
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $input_value = htmlspecialchars($_POST['input_text']);
+        if (empty($input_value)) {
+            echo "<br>no value to send<br>";
+        } else {
+            echo "<br>Comment : " . $input_value . "<br>";
+        }
+    }
+    ?>
 </body>
 </html>
